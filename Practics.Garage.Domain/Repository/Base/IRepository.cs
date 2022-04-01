37f261cc690d112eab5b741b8d8efbad32f42356
Bulat Sharipov <bulat20031006@gmail.com>
2022-04-01 20:53:48 +0300

    - Интерфейс репозитория

---
+++ Practics.Garage.Domain/Repository/Base/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Practics.Garage.Domain.Models.Base;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practics.Garage.Domain.Repository.Base
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        public Task<TEntity> Create(TEntity entity);
+
+        public IQueryable<TEntity> Read();
+
+        public Task<TEntity> Update(TEntity entity);
+
+        public Task<bool> Delete(TEntity entity);
+    }
+}
